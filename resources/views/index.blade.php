62d8d7dde97610562a2f5e0b07b6da3620d60593
lexmagnus <lexmagnus1088@gmail.com>
2017-11-27 19:47:04 +0000

    using layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" type="text/css" href="/css/custom.css">
-    <script src="/js/custom.css"></script>
-</head>
-
-<body>      
-    <button onclick="topFunction()" id="btopo" title="homepagebutton">Top</button>
-            <div id="menu">
-              <ul>
-                <li><a href="#login">Login</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#free">Free-Trial</a></li>
-                <li><a href="#serv">Serviços</a></li>
-                <li><a href="#home">Home</a></li>
-              </ul>              
-            </div> 
-            
-            <form action="/formulario.php">
-              <div class="container">
-                <label><b>Username:</b></label>
-                <input type="text" placeholder="Username" name="user" required>
-            
-                <label><b>Password:</b></label>
-                <input type="password" placeholder="Password" name="pw" required>
-            
-                <button type="submit" id="lbutton">Login</button>
-                
-                <input type="checkbox" checked="checked"> Remember </br>
-                <span class="pw">Forgot <a href="#">your password?</a></span>
-              </div>
-            </form>
-
+@section('content')
             <section id="slide1" class="homeslide">
                 <div id="home">
                     <div class="bodycontainer">
@@ -70,6 +37,4 @@
                     </div>
                 </div>
             </section>
-</body>
-
-</html>
+@endsection
