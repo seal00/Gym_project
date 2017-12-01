225bedc8c92cc4f0476c06f3197fe9a65ac4ee63
lexmagnus <lexmagnus1088@gmail.com>
2017-12-01 08:42:16 +0000

    correcção username na tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->unique();
+            $table->string('username');
             $table->boolean('isAdmin')->default(0);
             $table->boolean('isInst')->default(0);
             $table->string('email')->unique();
