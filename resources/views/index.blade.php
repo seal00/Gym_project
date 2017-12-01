22811e9764bfb5787996ac78536567f0fc7f87f8
seal00 <32486746+seal00@users.noreply.github.com>
2017-12-01 14:01:05 +0000

    Menu de login está melhor mas ainda não está bom

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,9 +33,6 @@
                     <div class="aboutcontainer">
                         <div class="aboutcontent">
                             <h2>Sobre o nosso ginásio</h2>
-                            <p></p>
-                            <p></p>
-                            <p></p>
                             <p>Adaptado a qualquer idade e ao gosto de cada pessoa, os serviços são diversificados e visam dar resposta a todo o tipo de clientes, indo de encontro das necessidades e metas individuais.</p>
                             <p>Para quem prefere um treino mais individualizado , podem optar pelo serviço de PT onde envolve um acompanhamento, realizado de acordo com as necessidades de cada cliente e orientado por um profissional. Ter um treino personalizado é a forma mais indicada para atingir os seus objetivos a nível físico e psicológico, uma vez que todos nós temos necessidades próprias que terão de ser compreendidas nos diversos domínios por um profissional. ´</p>
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,74 +2,115 @@
 <html>
 
 <head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}" />
-    <script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/custom.js') }}"></script>
-    <script>
-        window.Laravel = <?php echo json_encode([
+	<title>{{ config('app.name', 'Laravel') }}</title>
+	<link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}" />
+	<script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script>
+	<script type="text/javascript" src="{{ URL::asset('js/custom.js') }}"></script>
+	<script>
+		window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
         ]); ?>
-    </script>
+	</script>
 </head>
 
 <body>
-    <button onclick="topFunction()" id="btopo" title="homepagebutton">Top</button>
-    <div id="menu">
-        <ul>
-            <li id="login"><a href="{{ url('/login') }}">Login</a>
+	<button onclick="topFunction()" id="btopo" title="homepagebutton">Top</button>
+	<div id="menu">
+		<ul>
+			<li id="login">
+				<!--<a href="{{ url('/login') }}">Login</a>-->
+				<a id="login-trigger" href="#">Login</a>
+			
+				<form id="login-form" role="form" method="POST" action="{{ url('/login') }}">
+					{{ csrf_field() }}
+					<label>
+						<b>Username:</b>
+					</label>
 
-            <form id="login-form" role="form" method="POST" action="{{ url('/login') }}">
-            {{ csrf_field() }}
-                <label><b>Username:</b></label>
+					<input type="text" placeholder="Username" id="username" type="username" class="form-control" name="username" required> @if ($errors->has('username'))
+					<span>
+						<strong>{{ $errors->first('username') }}</strong>
+						<br>
+					</span>
+					@endif
 
-                <input type="text" placeholder="Username" id="username" type="username" class="form-control" name="username" required>
-                @if ($errors->has('username'))
-                    <span>
-                        <strong>{{ $errors->first('username') }}</strong><br>
-                    </span>
-                @endif
+					<label>
+						<b>Password:</b>
+					</label>
+					<input type="password" placeholder="Password" class="form-control" name="password" required> @if ($errors->has('password'))
+					<span>
+						<strong>{{ $errors->first('password') }}</strong>
+						<br>
+					</span>
+					@endif
 
-                <label><b>Password:</b></label>
-                <input type="password" placeholder="Password" class="form-control" name="password" required>
-                @if ($errors->has('password'))
-                    <span>
-                        <strong>{{ $errors->first('password') }}</strong><br>
-                    </span>
-                @endif
+					<button type="submit" id="lbutton">Login</button>
 
-                <button type="submit" id="lbutton">Login</button>
+					<input type="checkbox" name="remember" {{ old( 'remember') ? 'checked' : ''}}> Remember </br>
+					<span>Forgot
+						<a id="forgot" href="{{ url('/password/reset') }}">Your Password?</a>
+					</span>
+					<span id="create">Not a member?
+						<a id="create_account" href="{{ url('/register') }}">
+							<br>Create an account free</a>
+					</span>
+				</form>
+			</li>
+			
 
-                <input type="checkbox" name="remember"  {{ old('remember') ? 'checked' : ''}}> Remember </br>
-                <span>Forgot <a id="forgot" href="{{ url('/password/reset') }}">Your Password?</a></span>
-                <span id="create">Not a member? <a id="create_account" href="{{ url('/register') }}"><br>Create an account free</a></span>
-            </form>
+			<li >
+				<a id="ab" href="#about">About</a>
+			</li>
+			<li>
+				<a href="#free">Free-Trial</a>
+			</li>
+			<li>
+				<a href="#serv">Serviços</a>
+			</li>
+			<li>
+				<a href="#home">Home</a>
+			</li>
+		</ul>
+	</div>
 
-            </li>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-            <li><a href="#about">About</a></li>
-            <li><a href="#free">Free-Trial</a></li>
-            <li><a href="#serv">Serviços</a></li>
-            <li><a href="#home">Home</a></li>
-        </ul>
-    </div>
+	<script>
+		
+		$("#login-trigger").hover(function() {
+			$( "#login-form" ).slideDown( "slow" );
+			
+			$('#login-form').hover(function(){
+				$(this).addClass('active');
+			}, function(){
+				$(this).removeClass('active');
+			})
+			
+			$('#login-form').mouseenter(function() {
+				$('#login-form').show();  
+				}).mouseleave(function() {      
+					if(!$('#login-form').hasClass('active') ){
+						$('#login-form').slideUp( "slow" );
+					}
+			});
+			
+		});
+		
+		$("#ab").hover(function() {
+			$( "#login-form" ).slideUp( "slow" );
+		});
+		
+	</script>
 
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-<script>
-    $("#login").hover(function() {
-    $("#login-form").slideToggle("slow");
-});
-</script>
-
-    @yield('content')
+	@yield('content')
 
 </body>
+
 </html>
